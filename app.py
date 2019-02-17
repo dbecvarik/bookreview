65bd388f282203c81a5689cf35226a461b66b58d
David Becvarik <dbecvari@redhat.com>
2019-02-17 18:08:25 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import connexion
+
+
+BOOKS = {'1984': 5,
+         'foo': 1,
+         'bar': 2,
+         }
+
+def get_review(name):
+    if name in BOOKS:
+        return BOOKS[name]
+    raise Exception("Invalid book name specified")
+
+
+if __name__== '__main__':
+    app = connexion.App(__name__, specification_dir='swagger/')
+    app.add_api('review.yaml')
+    app.run(port=8080)
+
+
+ 
